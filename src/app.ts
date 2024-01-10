7dc2f570b93fc46fba82549595d29ed7aa56e363
matt <143686761+ulixert@users.noreply.github.com>
2024-01-10 04:54:21 -0800

    Add Prisma migration files and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
 import express, { Express } from 'express';
+import * as process from 'process';
+
+import { PrismaClient } from '@prisma/client';
 
 export const app: Express = express();
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.findUnique({
+    where: {
+      name_age: {
+        name: 'Alice',
+        age: 24,
+      },
+    },
+  });
+  console.log(user);
+  process.exit(0);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
